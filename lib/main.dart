8adf59687d938d9b46004e1c278dfa0e8c1ebda6
fairuzridh <fairuz.ridhfadh@gmail.com>
2024-12-02 13:35:53 +0700

    04.build wisata app first page

--- lib/main.dart
+++ lib/main.dart
@@ -9,101 +9,74 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Heading(text: "Hello World"), //text in title use HEADING()
-          actions: [
-            IconButton(
-              icon: const Icon(
-                Icons.search,
-                color: Colors.white,
-              ),
-              onPressed: () {},
-              )
-          ],
-          leading: IconButton(
-            icon: const Icon(
-              Icons.menu,
-              color: Colors.white,
-            ),
-            onPressed: () {}
-          ),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+      home: const DesainScreen(),
+    );
+  }
+}
+
+class DesainScreen extends StatelessWidget {
+  const DesainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            BiggerText(text: "fufu"),
-          ]
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                "Bojso Beach & Snow park",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold
+                ),
+                ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 24.0),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8.0,), //Jarak icon & text
+                      Text("Open Everyday", style: TextStyle(fontSize: 12.0),)
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.hourglass_bottom),
+                      SizedBox(height: 8.0,),
+                      Text("09.00 AM", style: TextStyle(fontSize: 12.0),)
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(height: 8.0,),
+                      Text("Cheapest", style: TextStyle(fontSize: 12.0),)
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                "Bojongsoang is now a densely populated sub-district and is quite developing rapidly considering its strategic position and its distance is quite close to the city of Bandung. With the plan to build a new city of Tegalluar and a high-speed train in Indonesia in the village of Tegalluar located at the eastern end of Bojongsoang district, it is certain that the development of Bojongsoang in the future will be more rapid even though the negative impact is the reduction of vacant land and rice fields as well as the rate of urbanization that may be uncontrollable",
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            )
+          ],
         ),
-        // floatingActionButton: FloatingActionButton(
-        //   child: const Icon(Icons.add),
-        //   onPressed: () {}
-        // ),
-      ),
-    );
-  }
-}
-
-//StatelessWidget for make HEADING()
-class Heading extends StatelessWidget {
-  final String text;
-  const Heading({super.key, required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.white
-      ),
-    );
-  }
-}
-
-
-//StatefulWidget for make BiggerText()
-class BiggerText extends StatefulWidget {
-  final String text;
-  const BiggerText({super.key, required this.text});  // const BiggerText({Key? key, required this.text}) : super(key: key);
-
-  @override
-  State<BiggerText> createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          widget.text, 
-          style: TextStyle(fontSize: _textSize)
-        ),
-        ElevatedButton(
-          child: const Text("Perbesar"),
-          onPressed: () {
-            setState(() {
-              _textSize += 8.0;
-            });
-          }
-        ),
-        ElevatedButton(
-          child: const Text("Reset"),
-          onPressed: () {
-            setState(() {
-              _textSize = 16.0;
-            });
-          }
-        )
-      ],
+      )
     );
   }
 }
